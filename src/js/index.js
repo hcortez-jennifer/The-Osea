6380954a0404c60ba38303478011e467c9caca42
hcortez-jennifer <117784015+hcortez-jennifer@users.noreply.github.com>
2025-11-04 22:12:30 -0500

    Added exhibition section

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,11 @@
 import * as bootstrap from 'bootstrap';
+
 import { headerAnimation } from './layouts/hero';
+import { exhibitionAnimation } from './layouts/exhibition';
 
 console.clear(); 
 
 document.addEventListener("DOMContentLoaded", () => {
     headerAnimation();
+    exhibitionAnimation();
 })
---
+++ src/js/layouts/exhibition.js
@@ -0,0 +1,51 @@
+import { gsap } from "gsap";
+import { MotionPathPlugin } from "gsap/MotionPathPlugin";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+gsap.registerPlugin(MotionPathPlugin, ScrollTrigger);
+
+// Exhibition animation
+ const segments = [
+    {start: 0, end: 0.2, card: 1},
+    {start: 0.2, end: 0.5, card: 2},
+    {start: 0.5, end: 0.75, card: 3},
+    {start: 0.75, end: 1, card: 4}
+];
+
+function showCard(num) {
+    gsap.to(".exhibition__card", {
+        opacity: 0, 
+        duration: 0.2
+    });
+    gsap.to(`.card${num}`, {
+        opacity: 1, 
+        duration: 0.5
+    });
+}
+
+export function exhibitionAnimation(){
+    const submarineTl = gsap.timeline({
+        scrollTrigger: {
+            trigger: ".exhibition__scroll",
+            start: "top top",
+            scrub: true,
+            pin: true,
+            markers: false
+        }
+    });
+
+    segments.forEach(({start, end, card}) => {
+        submarineTl.to(".submarine", {
+            motionPath: {
+            path: ".path",
+            align: ".path",
+            alignOrigin: [0.5, 0.5],
+            autoRotate: true,
+            start,
+            end
+          },
+          onStart: () => showCard(card)
+        });
+    });
+
+    showCard(num);
+}
